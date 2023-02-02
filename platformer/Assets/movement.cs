0d5e2597830445a254bcd29c015d3602a716f1f1
vishnuindukuri05 <73192429+vishnuindukuri05@users.noreply.github.com>
2023-02-02 14:23:21 -0600

    fuel bar

--- platformer/Assets/movement.cs
+++ platformer/Assets/movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class movement : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class movement : MonoBehaviour
     ParticleSystem right;
     ParticleSystem up;
     ParticleSystem foraward;
+    [SerializeField] Slider fuelbar;
     void Start()
     {
         fuel = 10;
@@ -17,6 +19,7 @@ public class movement : MonoBehaviour
         right = gameObject.transform.GetChild(3).gameObject.GetComponent<ParticleSystem>();
         up = gameObject.transform.GetChild(4).gameObject.GetComponent<ParticleSystem>();
         foraward = gameObject.transform.GetChild(5).gameObject.GetComponent<ParticleSystem>();
+        Physics.gravity = new Vector3(0, -0.5f, 0);
     }
 
     // Update is called once per frame
@@ -24,7 +27,7 @@ public class movement : MonoBehaviour
     {
             if(Input.GetKey(KeyCode.Space) && fuel >0){
                 fuel -= Time.fixedDeltaTime;
-                gameObject.GetComponent<Rigidbody>().AddForce(transform.forward, ForceMode.Impulse);
+                gameObject.GetComponent<Rigidbody>().AddForce(transform.forward/5, ForceMode.Impulse);
                 foraward.Play();
             }
             else {
@@ -32,7 +35,7 @@ public class movement : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.W) && fuel >0 ){
                 fuel -= Time.fixedDeltaTime;
-                gameObject.GetComponent<Rigidbody>().AddForce(-transform.up, ForceMode.Impulse);
+                gameObject.GetComponent<Rigidbody>().AddForce(-transform.up/5, ForceMode.Impulse);
                 up.Play();
             }
             else {
@@ -40,7 +43,7 @@ public class movement : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.A) && fuel >0){
                 fuel -= Time.fixedDeltaTime/2;
-                gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0,-2,0));
+                gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0,-1,0));
                 right.Play();
             }
             else {
@@ -48,11 +51,15 @@ public class movement : MonoBehaviour
             }
             if (Input.GetKey(KeyCode.D) && fuel >0){
                 fuel -= Time.fixedDeltaTime/2;
-                gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0,2,0));
+                gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0,1,0));
                 left.Play();
             }
             else {
                 left.Stop();
             }
+            fuelbar.value = fuel;
+    }
+    private void OnGUI() {
+        GUI.Label(new Rect(175, 25, 500, 100), "Fuel left");
     }
 }
