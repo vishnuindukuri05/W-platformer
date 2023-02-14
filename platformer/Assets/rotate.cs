f052f03125eb722d5ff05f6b8e50ea20692ce784
vishnuindukuri05 <73192429+vishnuindukuri05@users.noreply.github.com>
2023-02-14 14:35:27 -0600

    more lvel design

--- platformer/Assets/rotate.cs
+++ platformer/Assets/rotate.cs
@@ -7,7 +7,7 @@ public class rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3 (0,0.01f,0);
     }
 
     // Update is called once per frame
