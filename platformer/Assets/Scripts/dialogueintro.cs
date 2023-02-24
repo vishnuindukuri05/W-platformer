dff6ae357ff95d9682a3c38b747291caa3cc0785
k1627107 <pyz4577@gmail.com>
2023-02-24 14:12:57 -0600

    Does it remind you of your family and friends?

--- platformer/Assets/Scripts/dialogueintro.cs
+++ platformer/Assets/Scripts/dialogueintro.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
@@ -8,14 +9,32 @@ public class dialogueintro : MonoBehaviour
     // Start is called before the first frame update
     public TextMeshProUGUI textComponent;
     public string[] lines;
+    public Animator animator;
     public float textSpeed;
     private int index;
+    private int levelToLoad;
+
 
     void Start()
     {
         textComponent.text = string.Empty;
         StartDialogue();
     }
+
+    public void FadeToNextLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -30,6 +49,10 @@ public class dialogueintro : MonoBehaviour
             {
                 StopAllCoroutines();
                 textComponent.text = lines[index];
+            }
+            if(index >= 11)
+            {
+                FadeToNextLevel();
             }
         }
     }
