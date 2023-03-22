2cc9ee55d277557c94143ba5f4d9ec82eab2e2bf
Ibigbelly54 <93939999+Ibigbelly54@users.noreply.github.com>
2023-03-22 14:31:27 -0500

    BRUB....... COLOR + - ;

--- platformer/Assets/Scripts/InteractionSystem/NPC_Talk.cs
+++ platformer/Assets/Scripts/InteractionSystem/NPC_Talk.cs
@@ -1,17 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class NPC_Talk : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _prompt;
 
+    private PostProcessVolume _postProcessVolume;
+    private ColorGrading _cr;
+
+    private bool on = false;
+    private int color = 0;
+    private bool brub;
+
     public string InteractionPrompt => _prompt;
+
+    private void Start() 
+    {
+        _postProcessVolume = GameObject.Find("Player_Camera").GetComponent<PostProcessVolume>();
+        _postProcessVolume.profile.TryGetSettings(out _cr);
+        brub = true;
+    }
+
     public bool Interact(Interactor interactor)
     {
         // put command to do here (maybe trigger dialogue?)
         Debug.Log("Initiate Dialogue");
+        on = true;
         // GetComponent<scriptName>().triggerDialogue; or something like that
         return true;
+    }
+
+    private void Update() {
+        if(on) {
+            if (brub){
+                color++;
+            }
+            else{
+                color--;
+            }
+            if(color < -180) {
+                brub = true;
+            }
+            if(color > 180) {
+                brub = false;
+            }
+        }
+        _cr.hueShift.value = color;
     }
 }
