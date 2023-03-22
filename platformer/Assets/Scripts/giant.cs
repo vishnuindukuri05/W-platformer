1251fdce1f077cedf1c4a668c04b4c366b5b6c60
vishnuindukuri05 <73192429+vishnuindukuri05@users.noreply.github.com>
2023-03-22 14:24:48 -0500

    giants

--- platformer/Assets/Scripts/giant.cs
+++ platformer/Assets/Scripts/giant.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class giant : MonoBehaviour
 {
     [SerializeField] GameObject pla;
+    [SerializeField] RuntimeAnimatorController run;
+    [SerializeField] RuntimeAnimatorController idle;
+    [SerializeField] RuntimeAnimatorController stomp;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,15 @@ public class giant : MonoBehaviour
     void Update()
     {
         if ((gameObject.transform.position - pla.transform.position).magnitude < 30){
-            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Vector3.Normalize(pla.transform.position - gameObject.transform.position).x*3, 0, Vector3.Normalize(pla.transform.position - gameObject.transform.position).z*3);
+            gameObject.transform.LookAt(pla.transform);
+            if ((gameObject.transform.position - pla.transform.position).magnitude > 5){
+                gameObject.GetComponent<Animator>().runtimeAnimatorController = run;
+                gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Vector3.Normalize(pla.transform.position - gameObject.transform.position).x*2, 0, Vector3.Normalize(pla.transform.position - gameObject.transform.position).z*2);
+            }
+            else {
+                gameObject.GetComponent<Animator>().runtimeAnimatorController = stomp;
+                gameObject.GetComponent<Rigidbody>().velocity = new Vector3(Vector3.Normalize(pla.transform.position - gameObject.transform.position).x*0.1f, 0, Vector3.Normalize(pla.transform.position - gameObject.transform.position).z*0.1f);
+            }
         }
         else {
             gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
